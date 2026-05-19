63d50ba1288ba4a9bb322177c31642c5ea64b523
defcrash <crash@sapo.pt>
2026-05-19 18:47:59 +0100

    Update wallabot.py

--- wallabot.py
+++ wallabot.py
@@ -8,7 +8,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import NoSuchElementException
-from webdriver_manager.chrome import ChromeDriverManager
 
 load_dotenv()
 
