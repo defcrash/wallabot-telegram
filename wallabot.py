48f0ff8736023af48d2560e108463985497da797
defcrash <crash@sapo.pt>
2026-05-19 18:43:13 +0100

    Update wallabot.py

--- wallabot.py
+++ wallabot.py
@@ -53,8 +53,11 @@ def get_listings(url):
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-
-    driver = webdriver.Chrome(options=options)
+    
+    # Força o Selenium a ler o driver nativo do Linux instalado pelo apt-get
+    service = Service(executable_path="/usr/bin/chromedriver")
+    
+    driver = webdriver.Chrome(service=service, options=options)
     driver.get(url)
     time.sleep(10)
 
